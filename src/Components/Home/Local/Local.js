52f78b3ba1350cfd6d46ec342ed74342a86540cd
Asurakun1 <udon3699@gmail.com>
2022-12-25 15:44:05 -0800

    more article pages have been added

--- src/Components/Home/Local/Local.js
+++ src/Components/Home/Local/Local.js
@@ -5,17 +5,17 @@ const Local = () =>{
         <section className="local">
             <h4>Local</h4>
             <ul>
-                <li><Link className='link'>Local Man Robs Bank, Shocked to Find Out It's Illegal</Link></li>
+                <li><Link to='/local/man-robs-bank' className='link'>Local Man Robs Bank, Shocked to Find Out It's Illegal</Link></li>
                 <br/>
-                <li><Link className='link'>Man Thinks SWAT Team Raiding His Home is Actually a Surprise Birthday Party, Gets Tased and Handcuffed</Link></li>
+                <li><Link to='/local/swat-team-suprise-party' className='link'>Man Thinks SWAT Team Raiding His Home is Actually a Surprise Birthday Party, Gets Tased and Handcuffed</Link></li>
                 <br/>
-                <li><Link className='link'>Outraged Man Demands to See Manager After Learning It's Actually Illegal to Steal From Local Pharmacy</Link></li>
+                <li><Link to='/local/outraged-man-demans-to-see-manager-after-pharmacy-robbery' className='link'>Outraged Man Demands to See Manager After Learning It's Actually Illegal to Steal From Local Pharmacy</Link></li>
                 <br/>
-                <li><Link className='link'>Local Shops Embrace the Future by Replacing Employees with Robots: "They Never Complain and They Don't Need Breaks"</Link></li>
+                <li><Link to='/local/they-took-our-jobs' className='link'>Local Shops Embrace the Future by Replacing Employees with Robots: "They Never Complain and They Don't Need Breaks"</Link></li>
                 <br/>
-                <li><Link className='link'>Local Man Brags About Being 'Isekai'd' into Fantasy World, Returns with Terrible Manuscript and No Marketable Skills</Link></li>
+                <li><Link to='/local/isekai' className='link'>Local Man Brags About Being 'Isekai'd' into Fantasy World, Returns with Terrible Manuscript and No Marketable Skills</Link></li>
                 <br/>
-                <li><Link className='link'>Man Loses Life Savings at Casino, Calls it 'Smart Financial Move' and Demands to See Manager</Link></li>
+                <li><Link to='/local/gambler-loses-everything-demands-to-see-manager' className='link'>Man Loses Life Savings at Casino, Calls it 'Smart Financial Move' and Demands to See Manager</Link></li>
                 <br/>
             </ul>
         </section>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,6 +5,8 @@ import Error404 from '../Error/Error';
 import Article0 from '../ArticleSection/Articles/Article-0';
 import Article1 from '../ArticleSection/Articles/Article-1';
 import { useState, useEffect } from "react";
+import Article2 from '../ArticleSection/Articles/Article-2';
+import Article3 from '../ArticleSection/Articles/Article-3';
 const NavBar = () => {
     const [rawData, setRawData] = useState("Loading...");
 
@@ -28,6 +30,8 @@ const NavBar = () => {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/random-article" element={<Article0 data={rawData[0]}/>} />
                 <Route path="/craft-Beer" element={<Article1 data={rawData[1]}/>} />
+                <Route path="/who-Stress-Myth" element={<Article2 data={rawData[2]}/>} />
+                <Route path="/trump-wall-around-sun" element={<Article3 data={rawData[3]}/>} />
                 <Route path="*" element={<Error404/>}/>
             </Routes>
         </Router>
