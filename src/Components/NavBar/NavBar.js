83ef1d925d4e1dfc83ec404aac71c6de7a3b79b4
Asurakun1 <udon3699@gmail.com>
2022-12-29 11:34:14 -0800

    including more content

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -32,6 +32,9 @@ const NavBar = () => {
                 <Route path="/global-economic-collapse" element={<Article0 data={rawData[4]}/>} />
                 <Route path="/government-replace-all-forms-of-communication" element={<Article0 data={rawData[5]}/>}/>
                 <Route path="/study-shows-that-stupid-can-get-even-stupider" element={<Article0 data={rawData[6]}/>}/>
+                <Route path="/local/man-robs-bank" element={<Article0 data={rawData[7]}/>} />
+                <Route path="/local/swat-team-suprise-party" element={<Article0 data={rawData[8]}/>} />
+                <Route path='/local/outraged-man-demans-to-see-manager-after-pharmacy-robbery' element={<Article0 data={rawData[9]}/>}/>
                 <Route path="*" element={<Error404/>}/>
             </Routes>
         </Router>
