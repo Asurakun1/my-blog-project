c94ea6c8c249562037a68c07383c1f6475640dd0
Asurakun1 <udon3699@gmail.com>
2022-12-30 19:57:43 -0800

    fix

--- src/Components/ArticleSection/Articles/Article-0.js
+++ src/Components/ArticleSection/Articles/Article-0.js
@@ -1,36 +1,14 @@
 import Author0 from '../Author/Author0';
 import ReactMarkdown from 'react-markdown';
-import { useState, useEffect } from 'react';
 const Article0 = (props) => {
-    const [data, setData] = useState();
-
-
-    useEffect(() => {
-        const dataToFetch = async () => {
-            try {
-                const response = await fetch('/data/articles.JSON');
-                const jsonResponse = await response.json();
-                setData(jsonResponse);
-            } catch (e) {
-                console.log(e);
-            }
-        }
-
-        setTimeout(() => {
-            dataToFetch();
-        }, 1000);
-    }, [])
-
 
     const loading = "Loading...";
-
-    console.log(props.data.id, data)
     return (
         <section className="article">
             <Author0 />
-            <h1 className="article-title">{data ? <ReactMarkdown>{props.data.title}</ReactMarkdown> : loading}</h1>
+            <h1 className="article-title">{props.data ? <ReactMarkdown>{props.data.title}</ReactMarkdown> : loading}</h1>
             {
-                data ?
+                props.data ?
                     <article>
                         <div>
                             {
