a3c67027bfa73c84657923c2778aca3c37f69f4f
Asurakun1 <udon3699@gmail.com>
2023-01-04 21:30:40 -0800

    Articles and headlines are no longer hardcoded

--- src/Components/Home/Local/Local.js
+++ src/Components/Home/Local/Local.js
@@ -1,26 +1,25 @@
 import './Local.css';
 import { Link } from 'react-router-dom';
-const Local = () =>{
-
+const Local = (props) => {
+    const localArticles = props.data.filter((element) => {
+        return element.articleType === 'local';
+    });
     const handleClick = () => {
-        window.scrollTo(0,0);
+        window.scrollTo(0, 0);
     }
-    return(
+    return (
         <section className="local">
             <h4>Local</h4>
             <ul>
-                <li><Link to='article-8' onClick={handleClick} className='link'>Local Man Robs Bank, Shocked to Find Out It's Illegal</Link></li>
-                <br/>
-                <li><Link to='article-9' onClick={handleClick} className='link'>Man Thinks SWAT Team Raiding His Home is Actually a Surprise Birthday Party, Gets Tased and Handcuffed</Link></li>
-                <br/>
-                <li><Link to='article-10' onClick={handleClick} className='link'>Outraged Man Demands to See Manager After Learning It's Actually Illegal to Steal From Local Pharmacy</Link></li>
-                <br/>
-                <li><Link to='article-11' onClick={handleClick} className='link'>Local Shops Embrace the Future by Replacing Employees with Robots: "They Never Complain and They Don't Need Breaks"</Link></li>
-                <br/>
-                <li><Link to='article-12' onClick={handleClick} className='link'>Local Man Brags About Being 'Isekai'd' into Fantasy World, Returns with Terrible Manuscript and No Marketable Skills</Link></li>
-                <br/>
-                <li><Link to='article-13' onClick={handleClick} className='link'>Man Loses Life Savings at Casino, Calls it 'Smart Financial Move' and Demands to See Manager</Link></li>
-                <br/>
+                {
+                    localArticles.map((element, index) => {
+                        return <li key={index}>
+                            <Link to={`article-${element.id}`} onClick={handleClick} className='link'>{element.title}</Link>
+                            <p></p>
+                            <br/>
+                        </li>
+                    })
+                }
             </ul>
         </section>
     );
